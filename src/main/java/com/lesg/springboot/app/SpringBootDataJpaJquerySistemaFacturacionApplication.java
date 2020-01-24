0e443f776bea354eb654de7d6f65fcf987042e39
alphamen01 <lesg.2018@hotmail.com>
2020-01-24 00:11:09 -0500

    entidades ok

---
+++ src/main/java/com/lesg/springboot/app/SpringBootDataJpaJquerySistemaFacturacionApplication.java
@@ -0,0 +1,13 @@
+package com.lesg.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataJpaJquerySistemaFacturacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDataJpaJquerySistemaFacturacionApplication.class, args);
+	}
+
+}
